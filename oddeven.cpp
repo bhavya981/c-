f2419e1a119b085bd748f5ce5340d7be2ce2072b
bhavya981 <bhavyasharma981@gmail.com>
2025-03-26 14:32:49 +0530

    Add C++ source files and build configuration for development

---
+++ oddeven.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+     int number;
+     cout<<"Enter a number";
+     cin>>number;
+     if (number%2==0){
+        cout<<"the number "<<number<<" is even"<<endl;
+     } else{
+        cout<<"the number "<<number<<" is odd"<<endl;
+     }
+     return 0;
+    }
+       
