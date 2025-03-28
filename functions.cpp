9fb0a7a14f223ded57a1f9fa9976f3489f77750d
bhavya981 <bhavyasharma981@gmail.com>
2025-03-28 14:23:46 +0530

    abc

---
+++ functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+
+    int a=1;
+    double b=2.5;
+    char c='a';
+    string d="hello world";
+    bool e=false;
+
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+return 0;    
+}
